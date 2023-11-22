6baf6af9ed1f33e740aeb70f50da200df94273d0
LaxmiNk <klaxminaran@gmail.com>
2023-11-22 12:38:30 +0530

    Create date.js

---
+++ date.js
@@ -0,0 +1,8 @@
+// let myDate = new Date();
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2023, 0, 4);
+let myCreatedDate = new Date("01-6-2023");
+console.log(myCreatedDate.toLocaleString());
