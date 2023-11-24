594951573cb03145d5b427c08f3f739ab6d21453
LaxmiNk <klaxminaran@gmail.com>
2023-11-24 07:53:15 +0530

    Create array.js

---
+++ array.js
@@ -0,0 +1,20 @@
+//array
+const myArray = [1, 2, 3, 4, 4, 5, 6, `hii`];
+// console.log(myArray[2]);
+
+// array methods
+
+myArray.push(8);
+myArray.unshift(0);
+myArray.shift();
+// console.log(myArray.includes(4));
+// console.log(myArray.indexOf(3));
+
+// console.log(typeof myArray);
+// const newArr = myArray.join();
+// console.log(newArr);
+// console.log(typeof newArr);
+
+//splice and slice
+
+console.log(`A `, myArray);
