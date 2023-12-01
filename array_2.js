c91af5e189a2b5874a47e5c85bfa7c9c3e7dc53a
LaxmiNk <klaxminaran@gmail.com>
2023-12-01 19:42:39 +0530

    Create array_2.js
    array functions

---
+++ array_2.js
@@ -0,0 +1,10 @@
+const arr1 = [1, 2, 3, 4];
+const arr2 = [`hii`, `ji`, `kaise`, `ho`];
+// arr1.push(arr2);
+// console.log(arr1);
+// const all = arr1.concat(arr2);
+// all = [...arr1, ...arr2];
+// console.log(all);
+let newArr = [1, 2, 3, [5, 6], [8, 0, 9, [3, 4, 6]]];
+let plainArray = newArr.flat(Infinity);
+console.log(plainArray);
