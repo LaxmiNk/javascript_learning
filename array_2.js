5e9fcd11ee558f47abd4a1028801a97bd961c4a6
LaxmiNk <klaxminaran@gmail.com>
2023-12-01 21:11:07 +0530

    Update array_2.js

--- array_2.js
+++ array_2.js
@@ -8,3 +8,12 @@ const arr2 = [`hii`, `ji`, `kaise`, `ho`];
 let newArr = [1, 2, 3, [5, 6], [8, 0, 9, [3, 4, 6]]];
 let plainArray = newArr.flat(Infinity);
 console.log(plainArray);
+let name = `Laxminarayan`;
+console.log(Array.isArray(name));
+console.log(Array.from(name));
+let obj = {
+  name: `rohan`,
+  age: 23,
+};
+// console.log(Array.of({ name: `hitesh` }));
+console.log(Array.from(obj));
