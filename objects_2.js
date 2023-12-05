fc129052cb5d71b09b3db6638e21802831f2d7fa
LaxmiNk <klaxminaran@gmail.com>
2023-12-05 13:13:18 +0530

    Update objects_2.js

--- objects_2.js
+++ objects_2.js
@@ -16,5 +16,38 @@ const regularUser = {
 };
 // console.log(regularUser.fullname.userfullname.firstname);
 const obj1 = { 1: "a", 2: "b" };
-const obj2 = { 3: "c", 4: "d" };
-const obj3 = { }
+const obj2 = { 2: "c", 4: "d" };
+// const obj3 = { obj1, obj2 };
+// const obj3 = Object.assign({}, obj1, obj2);
+const obj3 = { ...obj1, ...obj2 };
+// console.log(obj3);
+// console.log(obj1);
+
+const users = [
+  {
+    id: 1,
+    email: "laxmi@gmail.com",
+  },
+];
+users[0].email;
+console.log(tinderUser);
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+console.log(tinderUser.hasOwnProperty("isLogged"));
+
+const course = {
+  name: "js in hindi",
+  price: 5900,
+  courseInstructor: "laxminarayan",
+};
+// course.courseInstructor
+const { courseInstructor: instructor } = course;
+console.log(instructor);
+
+// {
+//   "name":"laxminarayan",
+//   "courseName" : "js in hindi",
+//  "price":"free"
+
+// }
