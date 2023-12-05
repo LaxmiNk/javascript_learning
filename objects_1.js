282392b1fddc9582412976cd71378beb763894ab
LaxmiNk <klaxminaran@gmail.com>
2023-12-05 10:57:31 +0530

    Update objects_1.js

--- objects_1.js
+++ objects_1.js
@@ -1,8 +1,24 @@
 //singleton object
 
 //object literals
+const mySim = Symbol("key1");
 let person = {
   name: `laxminarayan`,
+  "full name": "Laxminarayan Kashyap",
   age: 18,
+  [mySim]: "mykey",
+  email: "laxmi@gmail.com",
+  location: "Bilaspur",
+  isloggedin: false,
+  lastLoginDay: ["Monday", "Tusday"],
 };
-console.log(person);
+// Object.freeze(person);
+person.email = "Narayan@gmail.com";
+person.greating = function () {
+  console.log("hello , person goodmornig");
+};
+person.greatingTwo = function () {
+  console.log(`hello , ${this["full name"]} goodmornig`);
+};
+console.log(person.greating());
+console.log(person.greatingTwo());
