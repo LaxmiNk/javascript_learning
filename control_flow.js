71b212703754285bb87d7dcfd6c1cf004e32a490
LaxmiNk <klaxminaran@gmail.com>
2023-12-06 21:44:06 +0530

    Create control_flow.js

---
+++ control_flow.js
@@ -0,0 +1,12 @@
+if (true) {
+  console.log("hii laxminarayan");
+}
+
+const num = 49;
+if (num <= 50) {
+  console.log("number is less than 50");
+} else if (num > 50) {
+  console.log("number is greater than 50");
+} else {
+  console.log("nothing is happen");
+}
