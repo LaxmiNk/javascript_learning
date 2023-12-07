d707fb911250b168546d59c2442dab7ad21f4f2a
LaxmiNk <klaxminaran@gmail.com>
2023-12-07 21:37:29 +0530

    Create loops_object.js

---
+++ loops_object.js
@@ -0,0 +1,16 @@
+const myObject = {
+  js: "javascript",
+  cpp: "c++",
+  rb: "rubi",
+  py: "python",
+};
+for (const key in myObject) {
+  //   console.log(myObject[key]);
+  //   console.log(`${key} shortcut for  ${myObject[key]}`);
+}
+
+const programing = ["java", "c++", "python", "rubi"];
+for (const key in programing) {
+  console.log(programing[key]);
+}
+
