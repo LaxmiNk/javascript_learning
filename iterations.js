a23987d77a6e4a9ae07ccbb1b17c07dde8eec3a8
LaxmiNk <klaxminaran@gmail.com>
2023-12-07 21:37:35 +0530

    Create iterations.js

---
+++ iterations.js
@@ -0,0 +1,18 @@
+// for
+// for (let i = 1; i <= 10; i++) {
+//   if (i == 5) {
+//     console.log("5 is the best number");
+//   }
+//   console.log(i);
+// }
+//Array specific loops
+const arr = [1, 2, 3, 4, 5, 6];
+// for of
+// for (const num of arr) {
+//   console.log(num);
+// }
+
+const greetings = "hello world";
+for (const greet of greetings) {
+  console.log(`each char is ${greet}`);
+}
